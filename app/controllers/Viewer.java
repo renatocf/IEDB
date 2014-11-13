c6f22d076fe0d51cec4284eb36143bcba0a07931
Renato Cordeiro Ferreira <renato.cferreira@hotmail.com>
2014-11-13 10:34:44 -0200

    Adds list of movies

--- app/controllers/Viewer.java
+++ app/controllers/Viewer.java
@@ -7,8 +7,10 @@ import models.Title;
 
 // Views
 import views.html.movie;
+import views.html.search_results;
 
 import java.util.List;
+import java.util.ArrayList;
 
 import play.*;
 import play.mvc.*;
@@ -18,10 +20,19 @@ public class Viewer extends Controller {
 
     public static Result find() {
         Title title = Form.form(Title.class).bindFromRequest().get();
-        return redirect(routes.Viewer.getMovie(title.getName()));
+        
+        List<Title> searchResults = new ArrayList<Title>();
+        searchResults.addAll(findMovies(title.getName()));
+        
+        return ok(search_results.render(searchResults));
     }
     
-    public static Result getMovie(String name) {
+    private static List<Movie> findMovies(String name) {
+        MovieDAO dao = new MovieDAO();
+        return dao.getByName(name.replace('-',' '));
+    }
+    
+    public static Result showMovie(String name) {
         MovieDAO dao = new MovieDAO();
         List<Movie> movies = dao.getByName(name.replace('-',' '));
         return ok(movie.render(movies.get(0).getName()));
