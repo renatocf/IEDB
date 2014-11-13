5b1ea6a1a7b6cf7c75249f016e6a1c599e0f58d6
Renato Cordeiro Ferreira <renato.cferreira@hotmail.com>
2014-11-13 08:32:28 -0200

    Basic mechanism working!

--- app/controllers/Viewer.java
+++ app/controllers/Viewer.java
@@ -3,6 +3,7 @@ package controllers;
 // Models
 import models.Movie;
 import models.MovieDAO;
+import models.Title;
 
 // Views
 import views.html.movie;
@@ -11,9 +12,15 @@ import java.util.List;
 
 import play.*;
 import play.mvc.*;
+import play.data.Form;
 
 public class Viewer extends Controller {
 
+    public static Result find() {
+        Title title = Form.form(Title.class).bindFromRequest().get();
+        return redirect(routes.Viewer.getMovie(title.getName()));
+    }
+    
     public static Result getMovie(String name) {
         MovieDAO dao = new MovieDAO();
         List<Movie> movies = dao.getByName(name.replace('-',' '));
