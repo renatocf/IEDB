2d39c23b68815355288ef4141194bf825d345a42
Renato Cordeiro Ferreira <renato.cferreira@hotmail.com>
2014-11-13 07:35:03 -0200

    Organizing classes

---
+++ test/integration/DBTest.java
@@ -0,0 +1,57 @@
+/**********************************************************************/
+/* Copyright 2014 IEDB                                                */
+/*                                                                    */
+/* Licensed under the Apache License, Version 2.0 (the "License");    */
+/* you may not use this file except in compliance with the License.   */
+/* You may obtain a copy of the License at                            */
+/*                                                                    */
+/*     http://www.apache.org/licenses/LICENSE-2.0                     */
+/*                                                                    */
+/* Unless required by applicable law or agreed to in writing,         */
+/* software distributed under the License is distributed on an        */
+/* "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,       */
+/* either express or implied.                                         */
+/* See the License for the specific language governing permissions    */
+/* and limitations under the License.                                 */
+/**********************************************************************/
+package integration;
+
+// Tested class
+import play.db.DB;
+import javax.sql.DataSource;
+import java.sql.Connection;
+
+// JUnit
+import org.junit.Before;
+import org.junit.After;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+// Fake application
+import static play.test.Helpers.running;
+import static play.test.Helpers.fakeApplication;
+
+public class DBTest {
+
+    @Test
+    public void testGetDataSource() {
+        running(fakeApplication(), new Runnable() {
+            @Override
+            public void run() {
+                DataSource ds = DB.getDataSource();
+                assertNotNull(ds);
+            }
+        });
+    }
+
+    @Test
+    public void testGetConnection() {
+        running(fakeApplication(), new Runnable() {
+            @Override
+            public void run() {
+                Connection conn = DB.getConnection();
+                assertNotNull(conn);
+            }
+        });
+    }
+}
---
+++ test/integration/MovieDAOTest.java
@@ -0,0 +1,54 @@
+/**********************************************************************/
+/* Copyright 2014 IEDB                                                */
+/*                                                                    */
+/* Licensed under the Apache License, Version 2.0 (the "License");    */
+/* you may not use this file except in compliance with the License.   */
+/* You may obtain a copy of the License at                            */
+/*                                                                    */
+/*     http://www.apache.org/licenses/LICENSE-2.0                     */
+/*                                                                    */
+/* Unless required by applicable law or agreed to in writing,         */
+/* software distributed under the License is distributed on an        */
+/* "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,       */
+/* either express or implied.                                         */
+/* See the License for the specific language governing permissions    */
+/* and limitations under the License.                                 */
+/**********************************************************************/
+package integration;
+
+// Tested class
+import models.MovieDAO;
+
+// JUnit
+import org.junit.Before;
+import org.junit.After;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+// Fake application
+import static play.test.Helpers.running;
+import static play.test.Helpers.fakeApplication;
+
+public class MovieDAOTest {
+    
+    private MovieDAO dao;
+
+    @Before
+    public void setUp() throws Exception{
+        running(fakeApplication(), new Runnable() {
+            public void run() {
+                dao = new MovieDAO();
+            }
+        });
+    }
+
+    @After
+    public void tearDown() throws Exception{
+        dao = null;
+    }
+
+    @Test
+    public void getByName() {
+        assertEquals("The Lord of the Rings: The Fellowship of the Ring", dao.getByName("Lord").get(0).getName());
+    }
+}
