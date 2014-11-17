2c03fbdbf5557740f23ef879a1b349198371b00f
Renato Cordeiro Ferreira <renato.cferreira@hotmail.com>
2014-11-16 23:24:02 -0200

    Partial refactoring of methods

--- app/models/TitleDAO.java
+++ app/models/TitleDAO.java
@@ -25,142 +25,116 @@ import java.sql.SQLException;
 
 import java.util.*;
 
-public class TitleDAO {
+public class TitleDAO extends DAO<Title> {
 
-    Connection connection;
-    
     public TitleDAO() {
-        this.connection = DB.getConnection();
+        super(DB.getConnection());
     }
     
     public List<Title> getAll() {
-        
-        String sql = "SELECT * FROM IEDB.Title ORDER BY id";
-        
-        try {
-            List<Title> titles = new ArrayList<Title>();
-            PreparedStatement stmt 
-                = this.connection.prepareStatement(sql);
-            
-            ResultSet rs = stmt.executeQuery();
-            while (rs.next()) {
-                Title title = new Title();
-                
-                title.setId(rs.getInt("id"));
-                title.setType(rs.getString("type"));
-                /* title.setCameFrom(rs.getInt("came_from")); */
-                title.setName(rs.getString("name"));
-                title.setDateCreation(rs.getDate("date_creation"));
-                title.setDescription(rs.getString("description"));
-                
-                titles.add(title);
-            }
-            rs.close();
-            
-            stmt.close();
-            return titles;
-            
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
-        }
+        return this.retrieveAllFromQuery(
+            "SELECT * FROM IEDB.Title ORDER BY id"
+        );
     }
 
-    public Title getTitleByTypeAndName(String type, String name){
-        String sql = "SELECT * "+
-                     "FROM IEDB.Complete_"+type.toLowerCase()+
-                     " WHERE lower(name) = ?";
-                     
-        try {
-            Title title = null;
-            PreparedStatement stmt 
-                = this.connection.prepareStatement(sql);
-
-            stmt.setString(1, name.replace("-", " ").toLowerCase());
-            ResultSet rs = stmt.executeQuery();
-            
-            rs.next();// maybe treat the case where its false?
-            switch(type.toLowerCase()){
-                case "music":
-                    Music music = new Music();
-                    music.setDuration(rs.getInt("duration"));
-                    title = music;
-                    break;
-                case "hq":
-                    Hq hq = new Hq();
-                    hq.setArc(rs.getString("arc"));
-                    hq.setNum(rs.getInt("num"));
-                    title = hq;
-                    break;
-                case "book":
-                    Book book = new Book();
-                    book.setNumEditions(rs.getInt("num_editions"));
-                    title = book;
-                    break;
-                case "movie":
-                    Movie movie = new Movie();
-                    movie.setNationality(rs.getString("nationality"));
-                    movie.setDuration(rs.getInt("duration"));
-                    //movie.setCensorship(rs.getString("censorship"));
-                    title = movie;
-                    break;
-                case "series":
-                    Series series = new Series();
-                    series.setDateInit(rs.getDate("date_init"));
-                    series.setDateEnd(rs.getDate("date_end"));
-                    series.setNumSeasons(rs.getInt("num_seasons"));
-                    //series.setCensorship(rs.getString("censorship"));
-                    title = series;
-                    break;
-                default:
-                    throw new RuntimeException();
-            }        
-
-            title.setId(rs.getInt("id"));
-            title.setType(rs.getString("type"));
-            title.setName(rs.getString("name"));
-            title.setDateCreation(rs.getDate("date_creation"));
-            title.setDescription(rs.getString("description"));
-            
-            rs.close();
-            stmt.close();
-            return title;
-        } catch (SQLException e) {
-            throw new RuntimeException();
+    public Title getTitleByTypeAndName(String type, String name) {
+        
+        MovieDAO dao = new MovieDAO();
+        switch(type.toLowerCase()) {
+            case "music":
+                return dao.getByName(name).get(0);
+            case "hq":
+                /* MovieDAO dao = new MovieDAO(); */
+                return dao.getByName(name).get(0);
+            case "book":
+                /* MovieDAO dao = new MovieDAO(); */
+                return dao.getByName(name).get(0);
+            case "movie": 
+                /* MovieDAO dao = new MovieDAO(); */
+                return dao.getByName(name).get(0);
+            case "series":
+                /* MovieDAO dao = new MovieDAO(); */
+                return dao.getByName(name).get(0);
+            default:
+                throw new RuntimeException("Invalid type " + type);
         }
     }
 
     public List<Title> getAllTitlesWithNameLike(String token){
-        List<Title> titles_id_type = this.getTitlesNameAndTypeWithNameLike(token);
-        List<Title> resp = new ArrayList<Title>();
-        for(Title title : titles_id_type)
-            resp.add(this.getTitleByTypeAndName(title.getType(), title.getName()));
+        
+        List<Title> partial = this.retrieveAllFromQuery(
+            "SELECT * FROM IEDB.Title WHERE lower(name) LIKE ?"
+        );
+        
+        List<Title> complete = new ArrayList<Title>();
+        for(Title title: partial)
+            complete.add(
+                getTitleByTypeAndName(title.getType(), title.getName())
+            );
 
-        return resp;
+        return complete;
     }
+    
+    @Override
+    protected Title buildFromResultSet(ResultSet rs) throws SQLException {
 
-    private List<Title> getTitlesNameAndTypeWithNameLike(String token){
-        String sql = " SELECT type, name"+
-                     " FROM   IEDB.Title"  +
-                     " WHERE  lower(name) LIKE ?";
-        List<Title> titles = new ArrayList<Title>();
-        Title new_title = null;
-
-        try{
-            PreparedStatement st = this.connection.prepareStatement(sql);
-            st.setString(1, "%"+token+"%");
-            ResultSet rs = st.executeQuery();
-
-            while(rs.next()){
-                new_title = new Title();
-                new_title.setName(rs.getString("name"));
-                new_title.setType(rs.getString("type"));
-                titles.add(new_title);
-            }
-        }catch(SQLException e){
-            throw new RuntimeException();
-        }
-        return titles;
+        Title title = new Title();
+        title.setId           (rs.getInt    ("id"));
+        title.setType         (rs.getString ("type"));
+        /* title.setCameFrom     (rs.getInt    ("came_from")); */
+        title.setName         (rs.getString ("name"));
+        title.setDateCreation (rs.getDate   ("date_creation"));
+        title.setDescription  (rs.getString ("description"));
+        return title;
+        
+        // switch ("movie") {
+        //     case "music":
+        //         Music music = new Music();
+        //         music.setId           (rs.getInt    ("id"));
+        //         music.setType         (rs.getString ("type"));
+        //         music.setName         (rs.getString ("name"));
+        //         music.setDateCreation (rs.getDate   ("date_creation"));
+        //         music.setDescription  (rs.getString ("description"));
+        //         music.setDuration     (rs.getInt    ("duration"));
+        //         return music;
+        //     case "hq":
+        //         Hq hq = new Hq();
+        //         hq.setId           (rs.getInt    ("id"));
+        //         hq.setType         (rs.getString ("type"));
+        //         hq.setName         (rs.getString ("name"));
+        //         hq.setDateCreation (rs.getDate   ("date_creation"));
+        //         hq.setDescription  (rs.getString ("description"));
+        //         hq.setArc          (rs.getString ("arc"));
+        //         hq.setNum          (rs.getInt    ("num"));
+        //         return hq;
+        //     case "book":
+        //         Book book = new Book();
+        //         book.setId           (rs.getInt    ("id"));
+        //         book.setType         (rs.getString ("type"));
+        //         book.setName         (rs.getString ("name"));
+        //         book.setDateCreation (rs.getDate   ("date_creation"));
+        //         book.setDescription  (rs.getString ("description"));
+        //         book.setNumEditions  (rs.getInt    ("num_editions"));
+        //         return book;
+        //     case "movie":
+        //         MovieDAO dao = new MovieDAO();
+        //         return dao.buildFromResultSet(rs);
+        //     case "series":
+        //         Series series = new Series();
+        //         series.setType         (rs.getString ("type"));
+        //         series.setName         (rs.getString ("name"));
+        //         series.setDateCreation (rs.getDate   ("date_creation"));
+        //         series.setDescription  (rs.getString ("description"));
+        //         series.setDateInit     (rs.getDate   ("date_init"));
+        //         series.setDateEnd      (rs.getDate   ("date_end"));
+        //         series.setNumSeasons   (rs.getInt    ("num_seasons"));
+        //         // series.setCensorship(rs.getString("censorship"));
+        //         return series;
+        //     default:
+        //         throw new RuntimeException();
+        // }
     }
+    
     /*
     private List<Title> getAllReferences(Title title){
 
