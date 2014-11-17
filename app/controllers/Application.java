a27dd24a374c62c3e4c3e99821effb263ba26623
ruan.cocito@gmail.com <ruan.cocito@gmail.com>
2014-11-17 19:21:41 -0200

    adds login. To see it, go to /login. Its not being used. YET!

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -2,6 +2,9 @@ package controllers;
 
 import play.*;
 import play.mvc.*;
+import play.data.*;
+import static play.data.Form.*;
+import models.Client;
 
 import views.html.*;
 
@@ -11,4 +14,33 @@ public class Application extends Controller {
         return ok(index.render("Hello world"));
     }
 
+	public static Result login() {
+	    return ok(login.render(Form.form(Login.class)));
+	}
+
+	public static Result authenticate() {
+    	Form<Login> loginForm = Form.form(Login.class).bindFromRequest();
+    	if (loginForm.hasErrors()) {
+    	 	return badRequest(login.render(loginForm));
+    	} 
+    	else {
+	      	session().clear();
+	        session("email", loginForm.get().email);
+	        return redirect(routes.Application.index());
+	    }
+	}
+
+	public static class Login {
+
+    	public String email;
+    	public String password;
+
+    	public String validate() {
+	    if (Client.authenticate(email, password) == null) {
+	    	return "Invalid user or password";
+	    }
+	    return null;
+	}
+
+	}
 }
