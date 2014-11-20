49fa8f70069e8a832348844e386b53656087a4e7
Renato Cordeiro Ferreira <renato.cferreira@hotmail.com>
2014-11-20 10:48:17 -0200

    Fixes identation

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -27,24 +27,27 @@ public class Application extends Controller {
     }
 
     //@Security.Authenticated(Secured.class)
-    public static Result addTitle(){
+    public static Result addTitle() {
     	return ok(addTitle.render());
     }
 
-
-    public static Result addMusic(){
+    public static Result addMusic() {
     	return ok(addMusic.render());
     }
-    public static Result addBook(){
+    
+    public static Result addBook() {
     	return ok(addBook.render());
     }
-    public static Result addComic(){
+    
+    public static Result addComic() {
     	return ok(addComic.render());
     }
-    public static Result addMovie(){
+    
+    public static Result addMovie() {
     	return ok(addMovie.render());
     }
-    public static Result addSeries(){
+    
+    public static Result addSeries() {
     	return ok(addSeries.render());
     }
 }
