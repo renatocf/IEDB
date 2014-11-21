3c3d050c640750b191e90ee79e036e83b6eeed41
Renato Cordeiro Ferreira <renato.cferreira@hotmail.com>
2014-11-21 15:52:35 -0200

    Uses helper class Form to validate form input

--- app/controllers/Register.java
+++ app/controllers/Register.java
@@ -28,19 +28,51 @@ import views.html.search_results;
 import play.data.Form;
 import play.mvc.Result;
 import play.mvc.Controller;
+import play.data.validation.ValidationError;
+
+// Java Util
+import java.util.List;
+import java.util.ArrayList;
 
 public class Register extends Controller {
 
+    final private static ClientDAO dao = new ClientDAO();
+
     public static Result signup() {
-        return ok(signup.render());
+        return ok(signup.render(Form.form(Register.NewClient.class)));
     }
 
     public static Result addClient() {
-        Client client = Form.form(Client.class).bindFromRequest().get();
-
-        ClientDAO dao = new ClientDAO();
-        dao.add(client);
-
-        return redirect(controllers.routes.Application.index());
+        Form<NewClient> clientForm
+            = Form.form(Register.NewClient.class).bindFromRequest();
+        
+    	if (clientForm.hasErrors())
+    	 	return badRequest(signup.render(clientForm));
+        
+        Register.dao.add(clientForm.get());
+        return redirect(routes.Application.index());
     }
+    
+	public static class NewClient extends Client {
+        
+        private String password_conf;
+            
+    	public List<ValidationError> validate() {
+            
+            List<ValidationError> errors = new ArrayList<>();
+            if(Register.dao.existsUsername(this.getUsername())) {
+                errors.add(new ValidationError(
+                    "username", "Username already exists!"));
+            }
+            if(Register.dao.existsEmail(this.getEmail())) {
+                errors.add(new ValidationError(
+                    "email", "Email already registered!"));
+            }
+            if(this.getPassword().equals(this.password_conf)) {
+                errors.add(new ValidationError(
+                    "password_conf", "Confirmation doesn't match!"));
+            }
+            return errors.isEmpty() ? null : errors;
+        }
+	}
 }
