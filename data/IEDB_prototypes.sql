8704bbab4cca367d053cc0f3f4da8b3d37b442bb
Renato Cordeiro Ferreira <renato.cferreira@hotmail.com>
2014-12-03 09:39:10 -0200

    Adds more useful prototypes

--- data/IEDB_prototypes.sql
+++ data/IEDB_prototypes.sql
@@ -5,11 +5,13 @@
 -- Prototype changes
 -- Naming convention: [u|i|d]_table_attribute
 
-INSERT INTO IEDB.Prototype_change
-VALUES ('u_title_description', 'IEDB.Title', 'UPDATE', 'id', 'description');
+-- Title
 INSERT INTO IEDB.Prototype_change
 VALUES ('u_title_name', 'IEDB.Title', 'UPDATE', 'id', 'name');
+INSERT INTO IEDB.Prototype_change
+VALUES ('u_title_description', 'IEDB.Title', 'UPDATE', 'id', 'description');
 
+-- Auditive/Visual/Written
 INSERT INTO IEDB.Prototype_change
 VALUES ('u_auditive_genre', 'IEDB.Auditive', 'UPDATE', 'id', 'genre');
 INSERT INTO IEDB.Prototype_change
@@ -19,18 +21,61 @@ VALUES ('u_visual_genre', 'IEDB.Visual', 'UPDATE', 'id', 'genre');
 INSERT INTO IEDB.Prototype_change
 VALUES ('u_visual_censorship', 'IEDB.Visual', 'UPDATE', 'id', 'censorship');
 
+-- Book
+INSERT INTO IEDB.Prototype_change
+VALUES ('u_book_name', 'IEDB.Title', 'UPDATE', 'id', 'name');
+INSERT INTO IEDB.Prototype_change
+VALUES ('u_book_description', 'IEDB.Title', 'UPDATE', 'id', 'description');
+INSERT INTO IEDB.Prototype_change
+VALUES ('u_book_genre', 'IEDB.Written', 'UPDATE', 'id', 'genre');
 INSERT INTO IEDB.Prototype_change
 VALUES ('u_book_num_editions', 'IEDB.Book', 'UPDATE', 'id', 'num_editions');
+
+-- Comic
+INSERT INTO IEDB.Prototype_change
+VALUES ('u_comic_name', 'IEDB.Title', 'UPDATE', 'id', 'name');
+INSERT INTO IEDB.Prototype_change
+VALUES ('u_comic_description', 'IEDB.Title', 'UPDATE', 'id', 'description');
+INSERT INTO IEDB.Prototype_change
+VALUES ('u_comic_genre', 'IEDB.Written', 'UPDATE', 'id', 'genre');
 INSERT INTO IEDB.Prototype_change
 VALUES ('u_comic_num', 'IEDB.Comic', 'UPDATE', 'id', 'num');
 INSERT INTO IEDB.Prototype_change
 VALUES ('u_comic_arc', 'IEDB.Comic', 'UPDATE', 'id', 'arc');
+
+-- Movie
+INSERT INTO IEDB.Prototype_change
+VALUES ('u_movie_name', 'IEDB.Title', 'UPDATE', 'id', 'name');
+INSERT INTO IEDB.Prototype_change
+VALUES ('u_movie_description', 'IEDB.Title', 'UPDATE', 'id', 'description');
+INSERT INTO IEDB.Prototype_change
+VALUES ('u_movie_genre', 'IEDB.Visual', 'UPDATE', 'id', 'genre');
+INSERT INTO IEDB.Prototype_change
+VALUES ('u_movie_censorship', 'IEDB.Visual', 'UPDATE', 'id', 'censorship');
 INSERT INTO IEDB.Prototype_change
 VALUES ('u_movie_duration', 'IEDB.Movie', 'UPDATE', 'id', 'duration');
 INSERT INTO IEDB.Prototype_change
 VALUES ('u_movie_nationality', 'IEDB.Movie', 'UPDATE', 'id', 'nationality');
+
+-- Music
+INSERT INTO IEDB.Prototype_change
+VALUES ('u_music_name', 'IEDB.Title', 'UPDATE', 'id', 'name');
+INSERT INTO IEDB.Prototype_change
+VALUES ('u_music_description', 'IEDB.Title', 'UPDATE', 'id', 'description');
+INSERT INTO IEDB.Prototype_change
+VALUES ('u_music_genre', 'IEDB.Auditive', 'UPDATE', 'id', 'genre');
 INSERT INTO IEDB.Prototype_change
 VALUES ('u_music_duration', 'IEDB.Music', 'UPDATE', 'id', 'duration');
+
+-- Series
+INSERT INTO IEDB.Prototype_change
+VALUES ('u_series_name', 'IEDB.Title', 'UPDATE', 'id', 'name');
+INSERT INTO IEDB.Prototype_change
+VALUES ('u_series_description', 'IEDB.Title', 'UPDATE', 'id', 'description');
+INSERT INTO IEDB.Prototype_change
+VALUES ('u_series_genre', 'IEDB.Visual', 'UPDATE', 'id', 'genre');
+INSERT INTO IEDB.Prototype_change
+VALUES ('u_series_censorship', 'IEDB.Visual', 'UPDATE', 'id', 'censorship');
 INSERT INTO IEDB.Prototype_change
 VALUES ('u_series_date_init', 'IEDB.Series', 'UPDATE', 'id', 'date_init');
 INSERT INTO IEDB.Prototype_change
